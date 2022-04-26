04947ff3e906c8d3321665a20536ad14664093d3
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-04-26 15:38:32 +0200

    define method to save user data to the server and test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'new User', age: 18 });
 
-user.fetch();
-
-setTimeout(() => {
-  console.log(user);
-}, 4000);
+user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -46,6 +46,18 @@ export class User {
       .get(`http://localhost:3000/users/${this.get('id')}`)
       .then((response: AxiosResponse): void => {
         this.set(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
       });
+  }
+  save(): void {
+    const id = this.get('id');
+
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post('http://localhost:3000/users', this.data);
+    }
   }
 }
