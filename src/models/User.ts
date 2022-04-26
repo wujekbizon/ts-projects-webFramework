20311b124d6a78618a9276d9add436d2c67fedb7
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-04-26 18:47:10 +0200

    finish refactor Sync class

--- src/models/User.ts
+++ src/models/User.ts
@@ -8,9 +8,11 @@ export interface UserProps {
   id?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
   public events: Eventing = new Eventing();
-  public sync: Sync;
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
   constructor(private data: UserProps) {}
 
