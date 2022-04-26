f0b80656089dc3a77e85898d907f6926f1d7a546
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-04-26 07:56:27 +0200

    define set method in User class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Greg', age: 35 });
+const user = new User({});
+
+user.set({ age: 99, name: 'newName' });
 
 console.log(user.get('name'));
 console.log(user.get('age'));
