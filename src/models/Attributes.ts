60244eaae51c66b6982160b65c6c21c26798d25c
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-04-26 19:58:53 +0200

    next commit

---
+++ src/models/Attributes.ts
@@ -0,0 +1,12 @@
+export class Attributes<T> {
+  constructor(private data: T) {}
+
+  // advanced generic constraint
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
+  }
+
+  set(update: T): void {
+    Object.assign(this.data, update);
+  }
+}
