3c08587505acb9847040430ed9358e781c6605d9
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-04-28 19:28:16 +0200

    add helper method to save user and test

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -6,8 +6,13 @@ export class UserForm extends View<User, UserProps> {
     return {
       'click:.set-age': this.onSetAgeClick,
       'click:.set-name': this.onSetNameClick,
+      'click:.save-user': this.onSaveClick,
     };
   }
+
+  onSaveClick = (): void => {
+    this.model.save();
+  };
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
@@ -25,14 +30,12 @@ export class UserForm extends View<User, UserProps> {
   template(): string {
     return `
     <div>
-      <h1>User Form</h1>
-      <div>User name:${this.model.get('name')} </div>
-      <div>User age:${this.model.get('age')} </div>
         <label for="name">Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" placeholder="${this.model.get('name')}" />
         <button class="set-name">Update Name</button>
         <br>
-      <button class="set-age">Set Random Age</button>
+        <button class="set-age">Set Random Age</button>
+        <button class="save-user">Save User</button>
     </div>
   `;
   }
